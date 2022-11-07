d24c34e41567514a5376e62cab355557a2a5b663
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2022-11-07 12:41:44 +0000

    airbnb_clone_v4

--- android/app/src/main/kotlin/kurztech/com/kin_music_player_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package kurztech.com.kin_music_player_app
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity()
